8cff58e68350b832191e8abb3c14a10f6469dd97
Sezgin Yurdakul <sezginyurdakul2022@gmail.com>
2022-08-24 19:51:18 +0200

    week2 Prep exercise is added

---
+++ Week2/QA-session-content/PrepExercise/createRecipeTables.js
@@ -0,0 +1,68 @@
+export const CREATE_TABLE_tbl_category = `
+CREATE TABLE IF NOT EXISTS tbl_category(
+category_Id VARCHAR(20) NOT NULL,
+category_Name VARCHAR(100) NOT NULL,
+PRIMARY KEY (category_Id));`;
+
+export const CREATE_TABLE_tbl_food_list = `
+CREATE TABLE IF NOT EXISTS tbl_food_list(
+food_Id VARCHAR(20) NOT NULL,
+food_Desc VARCHAR(500) NOT NULL,
+is_Available BOOLEAN NOT NULL,
+PRIMARY KEY (food_Id));`;
+
+export const CREATE_TABLE_tbl_unit_list = `
+CREATE TABLE IF NOT EXISTS tbl_unit_list(
+unit_Id VARCHAR(2) NOT NULL,
+unit_Description VARCHAR(10) NOT NULL,
+PRIMARY KEY (unit_Id));`;
+
+export const CREATE_TABLE_tbl_recipe = `
+CREATE TABLE IF NOT EXISTS tbl_recipe(
+recipe_Id VARCHAR(20) NOT NULL,
+recipe_Description VARCHAR(500) NOT NULL,
+recipe_Created_Date DATE NOT NULL,
+category1_Id VARCHAR(20) NOT NULL,
+category2_Id VARCHAR(20) NULL DEFAULT NULL,
+category3_Id VARCHAR(20) NULL DEFAULT NULL,
+servings_Number  INT NOT NULL,
+PRIMARY KEY (recipe_Id),
+CONSTRAINT tbl_recipe_ibfk_1
+  FOREIGN KEY (category1_Id)
+  REFERENCES tbl_category(category_Id),
+CONSTRAINT tbl_recipe_ibfk_2
+  FOREIGN KEY (category2_Id)
+  REFERENCES Tbl_category(category_Id),
+CONSTRAINT tbl_recipe_ibfk_3
+  FOREIGN KEY (category3_Id)
+  REFERENCES tbl_category(category_Id));`;
+
+export const CREATE_TABLE_tbl_ingredient_records = `
+CREATE TABLE IF NOT EXISTS tbl_ingredient_records(
+record_Id VARCHAR(20) NOT NULL,
+recipe_Id VARCHAR(20) NOT NULL,
+food_Id VARCHAR(20) NOT NULL,
+unit_Id VARCHAR(2) NOT NULL,
+amount  INT NOT NULL,
+PRIMARY KEY (record_Id),
+CONSTRAINT tbl_ingredient_records_ibfk_1
+  FOREIGN KEY (recipe_Id)
+  REFERENCES tbl_recipe (recipe_Id),
+CONSTRAINT tbl_ingredient_records_ibfk_2
+  FOREIGN KEY (food_Id)
+  REFERENCES tbl_food_list(food_Id),
+CONSTRAINT tbl_ingredient_records_ibfk_3
+  FOREIGN KEY (unit_Id)
+  REFERENCES tbl_unit_list(unit_Id));`;
+
+export const CREATE_TABLE_tbl_step_records = `
+CREATE TABLE IF NOT EXISTS tbl_step_records(
+record_Id VARCHAR(20) NOT NULL,
+recipe_Id VARCHAR(20) NOT NULL,
+step_Seq_No  INT NOT NULL,
+step_Definition VARCHAR(500) NOT NULL,
+step_Duration_Min  INT NOT NULL,
+PRIMARY KEY (record_Id),
+CONSTRAINT tbl_step_records_ibfk_1
+  FOREIGN KEY (recipe_Id)
+  REFERENCES tbl_recipe(recipe_Id));`;
